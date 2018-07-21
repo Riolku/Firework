e8e0386b566e0060d1b46e19c7b8ae41f2393c8d
gugek7502 <gugek7502@wrdsb.ca>
2018-07-21 11:06:38 -0400

    removed testfile and token_gen

--- token_gen.py
+++
@@ -1,7 +0,0 @@
-tokens = "` # ( ) [ ] { } : ; \\n \\\" \\\' \\\\\\\" \\\\\\\' , . ? ++ --".split(" ")
-operators = "~ ! @ $ % ^ & && * ** - + | || < << > >> / // =".split(" ")
-print("{",end="")
-enclose = lambda x: "\"%s\""%x
-print(",".join(map(enclose, tokens)),end=",")
-print(",".join(map(enclose, operators)),end=",")
-print(",".join(map(enclose, [a+"=" for a in operators])),end="}\n")
